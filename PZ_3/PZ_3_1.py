65aa0d3fc52d454080042422f66b802b0f805939
dzzzx1 <a.pirogova852007@gmail.com>
2024-12-07 15:46:50 +0300

    пз3 задание1 код

--- PZ_3/PZ_3_1.py
+++ PZ_3/PZ_3_1.py
@@ -1,9 +1,12 @@
 #Проверить истинность высказывания: «Среди трех данных целых чисел есть хотя бы одна пара совпадающих».
 while True:
-    num1 = int(input("Введите первое число:"))
-    num2 = int(input("Введите второе число:"))
-    num3 = int(input("Введите третье число:"))
-    if num1 == num2 or num1 == num3 or num2 == num3:
-        print(True)
-    else:
-        print(False)
+    try:
+        num1 = int(input("Введите первое число:"))
+        num2 = int(input("Введите второе число:"))
+        num3 = int(input("Введите третье число:"))
+        if num1 == num2 or num1 == num3 or num2 == num3:
+            print(True)
+        else:
+            print(False)
+    except:
+        print("Что то пошло не так")
