866ad065aa19da2986241b8e3cd02aa415d4dd29
dzzzx1 <a.pirogova852007@gmail.com>
2024-12-13 23:54:56 +0300

    пз 4 код

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -3,14 +3,17 @@
 # Полученное число является приближенным значением функции sin в точке X.
 x = float(input("Введите вещественное число X: "))
 n = int(input("Введите целое число N (> 0): "))
-if n <= 0:
-    print("Число должно быть больше нуля!")
-else:
-    total_sum = 0
-    for i in range(1, n + 1):
-        num = (-1)**(i - 1) * (x**(2 * i - 1))
-        num1 = 1
-        for j in range(1, 2 * i):
-            num1 *= j
-        total_sum += num / num1
-    print("Приближенное значение sin(x):", total_sum)
+try:
+    if n <= 0:
+        print("Число должно быть больше нуля!")
+    else:
+        total_sum = 0
+        for i in range(1, n + 1):
+            num = (-1)**(i - 1) * (x**(2 * i - 1))
+            num1 = 1
+            for j in range(1, 2 * i):
+                num1 *= j
+            total_sum += num / num1
+        print("Приближенное значение sin(x):", total_sum)
+except :
+    print("Что то пошло не так((( ")
--- PZ_4/PZ_4_2.py
+++ PZ_4/PZ_4_2.py
@@ -1,6 +1,9 @@
-# Дано целое число N (> 0). Найти сумму 1^N + 2^N-1 + ... + N^1.
+# Дано целое число N (> 0). Найти сумму 1^N + 2^N + ... + ..^N.
 N = int(input("Введите целое число N (> 0): "))
-total_sum = 0
-for i in range(1, N + 1):
-    total_sum += i ** (N - i + 1)
-print("Сумма:", total_sum)
+try:
+    total_sum = 0
+    for i in range(1, N ):
+        total_sum += i ** N
+    print("Сумма:", total_sum)
+except :
+    print("Что то пошло не так ((")
