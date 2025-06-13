8b6ea8acdfb6bb2946f7d392a9acfa666d28814a
dzzzx1 <a.pirogova852007@gmail.com>
2025-06-13 23:07:12 +0300

    пз 12 код

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -1,2 +1,13 @@
-# 1.В последовательности на n целых элементов найти среднее арифметическое
-# элементов первой трети.
+#В последовательности на n целых элементов найти произведение элементов
+#средней трети.
+
+import random
+from functools import reduce
+
+n = int(input("Введите количество элементов: "))
+a = [random.randint(1, 5) for i in range(n)]
+print(a)
+
+bbb = [a[b] for b in range (int((len(a) // 3) ), int((len(a) - len(a) // 3)))]
+print(bbb)
+print(reduce(lambda x,y: x * y, bbb))
--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -1,2 +1,10 @@
-# 2.Составить генератор (yield), который преобразует все буквенные символы в
-# заглавные.
+# Составить генератор (yield), который преобразует все буквенные символы в
+# строчные.
+
+text = 'TRUBODUR 899007 dEFgRjK'
+def zadanie(a):
+    b = a.split()
+    yield from [i.lower() for i in b]
+lower_text = zadanie(text)
+for i in lower_text:
+    print(i)
